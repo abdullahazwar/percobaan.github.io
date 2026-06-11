3f499d260e072dc3c39b9e64fadc991fc77effd9
azwar <251467440+abdullahazwar@users.noreply.github.com>
2026-06-11 21:06:51 +0700

    Add files via upload
    first deploy

---
+++ latihan/latian.js
@@ -0,0 +1,10 @@
+const nama='nama saya adalah Abdullah Azwar Anas';
+const food='makanan favorite saya adalah rendang';
+const alamat='saya tinggal di pringsewu'
+
+const fulldata= nama+' '+food+' '+alamat;
+
+console.log(nama);
+console.log(food);
+console.log(alamat);
+console.log(fulldata);
